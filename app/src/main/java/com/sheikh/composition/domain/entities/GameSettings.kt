bedc5c9659830a1fb9321af09a3451b66a3c7fb1
Sheikh <barsamaniya11@gmail.com>
2022-11-23 22:41:18 +0500

    Navigation between fragments implemented

--- app/src/main/java/com/sheikh/composition/domain/entities/GameSettings.kt
+++ app/src/main/java/com/sheikh/composition/domain/entities/GameSettings.kt
@@ -1,8 +1,10 @@
 package com.sheikh.composition.domain.entities
 
+import java.io.Serializable
+
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val timeInSeconds: Int
-)
+) : Serializable
--- app/src/main/java/com/sheikh/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/sheikh/composition/presentation/GameFragment.kt
@@ -1,17 +1,29 @@
 package com.sheikh.composition.presentation
 
+import android.media.MediaFormat.KEY_LEVEL
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.sheikh.composition.R
 import com.sheikh.composition.databinding.FragmentGameBinding
+import com.sheikh.composition.domain.entities.GameResult
+import com.sheikh.composition.domain.entities.GameSettings
+import com.sheikh.composition.domain.entities.Level
 
 class GameFragment : Fragment() {
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    private lateinit var level: Level
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        putArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +33,58 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvSum.setOnClickListener {
+            launchGameFinishedFragment()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun launchGameFinishedFragment() {
+        requireActivity()
+            .supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.main_container, GameFinishedFragment.newInstance(
+                    GameResult(
+                        true,
+                        10,
+                        20,
+                        GameSettings
+                            (
+                            20,
+                            10,
+                            50,
+                            20
+                        )
+                    )
+                )
+            )
+            .addToBackStack(GameFinishedFragment.FRAGMENT_NAME)
+            .commit()
+    }
+
+    private fun putArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object {
+
+        const val FRAGMENT_NAME = "GameFragment"
+
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
